aeefdee0fc16f257d1918f01594d91b30db80f14
kalpanamahto13 <kalpanmahto1310@gmail.com>
2025-05-16 14:20:10 +0530

    added tasks

---
+++ Assignment-1/task1.py
@@ -0,0 +1,11 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+
+add=a+b
+sub=a-b
+multiply=a*b
+div= a/b
+print(f"Addition of {a} and {b} = {add}")
+print(f"Subtraction of {a} and {b} = {sub}")
+print(f"Multiplication of {a} and {b} = {multiply}")
+print(f"Division of {a} and {b} = {div}")
---
+++ Assignment-1/task2.py
@@ -0,0 +1,5 @@
+firstName=input("enter your first name :")
+lastName=input("enter your last name:")
+
+fullname= firstName+ " "+ lastName
+print("Hello , "+ fullname + " ! welcome to the python program")
