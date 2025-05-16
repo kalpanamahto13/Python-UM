a535dd3f2eb3aaf113cccc0306e48e7606de73b8
kalpanamahto13 <kalpanmahto1310@gmail.com>
2025-05-16 14:40:59 +0530

    changed var

--- Assignment-3/task1.py
+++ Assignment-3/task1.py
@@ -5,6 +5,6 @@ def factorial(n):
     else:
         return n * factorial(n - 1)  # Recursive call
 
-# Call the function with a sample number and print the output
-sample_number = int(input("enter a number:"))
-print(f"The factorial of {sample_number} is: {factorial(sample_number)}")
+
+num = int(input("enter a number:"))
+print(f"The factorial of {num} is: {factorial(sample_number)}")
