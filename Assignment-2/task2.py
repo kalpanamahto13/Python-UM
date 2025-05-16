688ce61fb8e87611a8d76b01907e8026cef21dc7
kalpanamahto13 <kalpanmahto1310@gmail.com>
2025-05-16 14:29:10 +0530

    added assign 2 files

---
+++ Assignment-2/task2.py
@@ -0,0 +1,9 @@
+# Initialize sum variable
+total_sum = 0
+
+# Iterate over numbers from 1 to 50
+for num in range(1, 51):
+    total_sum += num  # Add each number to the sum
+
+# Display the final sum
+print("The sum of numbers from 1 to 50 is:", total_sum)
