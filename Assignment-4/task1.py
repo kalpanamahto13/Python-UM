7c3067b97a5020038be7f8c9ed8769882009e2c2
kalpanamahto13 <kalpanamahto1310@gmail.com>
2025-05-17 16:01:43 +0530

    Update task1.py

--- Assignment-4/task1.py
+++ Assignment-4/task1.py
@@ -4,5 +4,6 @@ try:
         # Read and print each line
         for line in file:
             print(line.strip())  # strip() removes leading/trailing whitespace
+            
 except FileNotFoundError:
     print("Error: The file 'sample.txt' was not found. Please check the filename and try again.")
